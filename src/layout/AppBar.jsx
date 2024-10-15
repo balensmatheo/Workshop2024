ea335191254b2c0fa847d42291bd0f5680ed09c3
Matheo <matheobalens@gmail.com>
2024-10-15 15:06:47 +0200

    add logout button

--- src/layout/AppBar.jsx
+++ src/layout/AppBar.jsx
@@ -15,19 +15,18 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { Drawer, List, ListItem, ListItemIcon, ListItemText, Divider, ListItemButton } from '@mui/material';
 import InboxIcon from '@mui/icons-material/Inbox';
-import { AccountCircleRounded, Dashboard, DashboardRounded, LeaderboardRounded } from '@mui/icons-material';
+import { AccountCircleRounded, DashboardRounded, LeaderboardRounded, Logout } from '@mui/icons-material';
+import { signOut } from 'aws-amplify/auth';
+
 
 export default function PrimarySearchAppBar() {
     const navigate = useNavigate();
-    
-    // State for the drawer
+
     const [drawerOpen, setDrawerOpen] = React.useState(false);
-    
-    // Handle Drawer open/close
+
     const toggleDrawer = (open) => (event) => {
-        // Check if it was triggered by a key press (Tab or Shift)
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-        return;
+            return;
         }
         setDrawerOpen(open);
     };
@@ -37,6 +36,10 @@ export default function PrimarySearchAppBar() {
 
     const handleProfileClick = () => {
         navigate('/me');
+    };
+
+    const handleLogout = async () => {
+        await signOut();
     };
 
     const handleMobileMenuClose = () => {
@@ -52,151 +55,156 @@ export default function PrimarySearchAppBar() {
 
     const renderMobileMenu = (
         <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-        }}
-        id={mobileMenuId}
-        keepMounted
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-        }}
-        open={isMobileMenuOpen}
-        onClose={handleMobileMenuClose}
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
         >
-        <MenuItem>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-            <Badge badgeContent={4} color="error">
-                <MailIcon />
-            </Badge>
-            </IconButton>
-            <p>Messages</p>
-        </MenuItem>
-        <MenuItem>
-            <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-            <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-            </Badge>
-            </IconButton>
-            <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={handleProfileClick}>
-            <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-            >
-            <AccountCircle />
-            </IconButton>
-            <p>Profile</p>
-        </MenuItem>
+            <MenuItem>
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={4} color="error">
+                        <MailIcon />
+                    </Badge>
+                </IconButton>
+                <p>Messages</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+                    <Badge badgeContent={17} color="error">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileClick}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
         </Menu>
     );
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer(true)}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Link to="/dashboard" style={{ textDecoration: 'none', color: 'white' }}>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ display: { xs: 'none', sm: 'block' } }}
+                        >
+                            The Positivity Calendar
+                        </Typography>
+                    </Link>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                            <Badge badgeContent={4} color="error">
+                                <MailIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+                            <Badge badgeContent={17} color="error">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileClick}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
 
-            <Toolbar>
-            {/* Toggle Drawer when MenuIcon is clicked */}
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                onClick={toggleDrawer(true)} // Open Drawer
-                sx={{ mr: 2 }}
+            <Drawer
+                open={drawerOpen}
+                onClose={toggleDrawer(false)}
+                sx={{
+                    width: 240,
+                    [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
+                }}
             >
-                <MenuIcon />
-            </IconButton>
-            <Link to="/dashboard" style={{ textDecoration: 'none', color: 'white' }}>
-                <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ display: { xs: 'none', sm: 'block' } }}
-                >
-                The Positivity Calendar
-                </Typography>
-            </Link>
-            <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="error">
-                    <MailIcon />
-                </Badge>
-                </IconButton>
-                <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-                <Badge badgeContent={17} color="error">
-                    <NotificationsIcon />
-                </Badge>
-                </IconButton>
-                <IconButton
-                    size="large"
-                    edge="end"
-                    aria-label="account of current user"
-                    aria-controls={menuId}
-                    aria-haspopup="true"
-                    onClick={handleProfileClick}
-                    color="inherit"
-                >
-                <AccountCircle />
-                </IconButton>
-            </Box>
-            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                    size="large"
-                    aria-label="show more"
-                    aria-controls={mobileMenuId}
-                    aria-haspopup="true"
-                    onClick={handleMobileMenuOpen}
-                    color="inherit"
-                >
-                <MoreIcon />
-                </IconButton>
-            </Box>
-            </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-
-        {/* Drawer component */}
-        <Drawer
-            open={drawerOpen}
-            onClose={toggleDrawer(false)}
-            sx={{
-                width: 240,
-                [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-            }}
-        >
-            <Toolbar />
-            <Box sx={{ overflow: 'auto' }}>
-                <List>
-                    {[
-                        {'title': 'Dashboard', 'path': '/dashboard', 'icon': <DashboardRounded />},
-                        {'title': 'Profile', 'path': '/me', 'icon': <AccountCircleRounded />},
-                        {'title': 'Leaderboard', 'path': '/leaderboard', 'icon': <LeaderboardRounded />},
-                    ].map((item, index) => (
-                    <ListItem key={item.title} >
-                        <ListItemButton onClick={() => navigate(item.path)}>
-                            <ListItemIcon sx={{ mr: 0 }}>
-                                {item.icon}
+                <Toolbar />
+                <Box sx={{ overflow: 'auto' }}>
+                    <List>
+                        {[
+                            {'title': 'Dashboard', 'path': '/dashboard', 'icon': <DashboardRounded />},
+                            {'title': 'Profile', 'path': '/me', 'icon': <AccountCircleRounded />},
+                            {'title': 'Leaderboard', 'path': '/leaderboard', 'icon': <LeaderboardRounded />},
+                        ].map((item) => (
+                            <ListItem key={item.title}>
+                                <ListItemButton onClick={() => navigate(item.path)}>
+                                    <ListItemIcon sx={{ mr: 0 }}>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.title} sx={{ ml: 0}} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    {/* Bouton de déconnexion rouge */}
+                    <ListItem>
+                        <ListItemButton onClick={handleLogout} sx={{ color: 'red' }}>
+                            <ListItemIcon sx={{ color: 'red', mr: 0 }}>
+                                <Logout />
                             </ListItemIcon>
-                            <ListItemText primary={item.title} sx={{ ml: 0}} />
+                            <ListItemText primary="Déconnexion" sx={{ ml: 0 }} />
                         </ListItemButton>
                     </ListItem>
-                    ))}
-                </List>
-                <Divider />
-            </Box>
-        </Drawer>
+                </Box>
+            </Drawer>
 
-        {/* This renders the routed content */}
-        <Toolbar />
-        <Outlet />
+            <Toolbar />
+            <Outlet />
         </Box>
     );
 }
