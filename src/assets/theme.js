889d0e3648bca3ea61e4d88c16b934f6f50c5fd7
TanguyDessoly <pro.tanguydessoly@gmail.com>
2024-10-15 08:25:34 +0200

    v0 init front

---
+++ src/assets/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#629624',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
+
+export default theme;
