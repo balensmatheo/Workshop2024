f273c9c47af3dbf010a7937401cfc2e9e80d2d0f
Matheo <matheobalens@gmail.com>
2024-10-15 14:07:10 +0200

    Add Vite Config

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { Amplify } from "aws-amplify"
-import outputs from "../amplify_outputs.json"
-
-Amplify.configure(outputs)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
