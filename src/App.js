44c99291edb8911de21918497182fec6915836f1
TanguyDessoly <pro.tanguydessoly@gmail.com>
2024-10-15 11:08:58 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -3,34 +3,48 @@ import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './assets/theme';
 import SignIn from './views/SignIn';
-import { ThemeProvider } from '@mui/material';
+import { AppBar, ThemeProvider, Toolbar } from '@mui/material';
 import {
+  BrowserRouter,
   createBrowserRouter,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
 import ErrorPage from './views/404';
 import Dashboard from './views/Dashboard';
+import PrimarySearchAppBar from './layout/AppBar';
+import UserProfile from './views/UserProfile';
 
 
 function App() {
 
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <SignIn />,
-      errorElement: <ErrorPage />,
+      path: "/signIn",
+      element: <SignIn />
     },
     {
-      path: "/app/dashboard",
-      element: <Dashboard />,
-    },
+      path: "/",
+      element: <PrimarySearchAppBar />,
+      errorElement: <ErrorPage />,
+      children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+          { 
+            path: "/me",
+            element: <UserProfile />,
+          }
+      ]
+    }
   ]);
 
   return (
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </>
   );
